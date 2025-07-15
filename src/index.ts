ba9be55736866c2d02315b2865947af39d53b1d5
rwzdoorn <53902901+rwzdoorn@users.noreply.github.com>
2025-07-15 14:23:27 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 export default {
-	async fetch(request): Promise<Response> {
-		const data = {
-			hello: "world",
-		};
-
-		return Response.json(data);
-	},
-} satisfies ExportedHandler;
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === "/.well-known/apple-app-site-association") {
+      const aasa = {
+        applinks: {
+          apps: [],
+          details: [
+            {
+              appID: "Z8TVS94HS8.com.dreamix-studio.runtrainer",
+              paths: [
+                "/reset-password/*",
+                "/deeplink=*",
+                ""
+              ]
+            }
+          ]
+        },
+        webcredentials: {
+          apps: ["Z8TVS94HS8.com.dreamix-studio.runtrainer"]
+        }
+      };
+      return new Response(JSON.stringify(aasa), {
+        headers: { "content-type": "application/json" }
+      });
+    }
+    return fetch(request);
+  }
+};
